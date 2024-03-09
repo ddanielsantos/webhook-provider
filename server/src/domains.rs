ca851a5d603fb229aefad5c1aaeb3696720e357d
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2024-03-09 18:06:09 -0300

    feat: create `SubscribeBody`

--- server/src/domains.rs
+++ server/src/domains.rs
@@ -1,9 +1,20 @@
 pub mod subscription {
-    use axum::{response::IntoResponse, routing::put, Router};
+    use axum::{extract::State, response::IntoResponse, routing::put, Router};
 
     use crate::state::AppState;
 
-    pub async fn subscribe_handler() -> impl IntoResponse {}
+    #[derive(serde::Deserialize)]
+    struct SubscribeBody {
+        subscribeTo: Vec<String>,
+        secret: String,
+        url: String,
+    }
+
+    async fn subscribe_handler(
+        State(state): State<AppState>,
+        axum::Json(body): axum::Json<SubscribeBody>,
+    ) -> impl IntoResponse {
+    }
 
     pub fn subscription_routes() -> Router<AppState> {
         Router::new().route("/subscribe", put(subscribe_handler))
