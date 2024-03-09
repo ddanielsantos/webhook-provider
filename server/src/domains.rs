1ed3493cad40e75600747fd62d1745f72f40c3bf
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2024-03-09 17:23:49 -0300

    feat: init `subscribe` endpoint

--- server/src/domains.rs
+++ server/src/domains.rs
@@ -1,1 +1,11 @@
+pub mod subscription {
+    use axum::{response::IntoResponse, routing::put, Router};
 
+    use crate::state::AppState;
+
+    pub async fn subscribe_handler() -> impl IntoResponse {}
+
+    pub fn subscription_routes() -> Router<AppState> {
+        Router::new().route("/subscribe", put(subscribe_handler))
+    }
+}
